81ca4a0b19937fc5a4ef7ddedbccb3f3e33a99c9
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-06-09 14:28:14 -0300

    Adicionado projeto WebAppe configurada página Home

---
+++ ControleDeBar.WebApp/Program.cs
@@ -0,0 +1,20 @@
+namespace ControleDeBar.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+        }
+    }
+}
