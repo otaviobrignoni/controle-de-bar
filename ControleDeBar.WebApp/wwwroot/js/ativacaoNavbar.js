494af39ee627bf9c7d5142a578d43ebd35ca9953
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-06-09 18:06:30 -0300

    Adicionado conteúdo da página inicial, refatorado estilos dos cards

---
+++ ControleDeBar.WebApp/wwwroot/js/ativacaoNavbar.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var currentPath = window.location.pathname.toLowerCase();
+    var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+    navLinks.forEach(function (link) {
+        var linkPath = link.getAttribute('href').toLowerCase();
+
+        if (currentPath === linkPath || currentPath.startsWith(linkPath + "/")) {
+            link.classList.remove('nav-link', 'text-primary');
+            link.classList.add('btn', 'btn-primary');
+        } else {
+            link.classList.remove('btn', 'btn-primary');
+            link.classList.add('nav-link', 'text-primary');
+        }
+    });
+});
