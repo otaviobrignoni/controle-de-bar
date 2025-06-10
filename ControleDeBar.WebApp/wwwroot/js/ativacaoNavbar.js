3a38f218031ff798cee82e1019d87be522278764
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-06-10 12:47:45 -0300

    Adicionados links de navegação da página Home

--- ControleDeBar.WebApp/wwwroot/js/ativacaoNavbar.js
+++ ControleDeBar.WebApp/wwwroot/js/ativacaoNavbar.js
@@ -2,15 +2,15 @@
     const enderecoAtual = window.location.pathname.toLowerCase();
     const linksNavbar = document.querySelectorAll('.navbar-nav .nav-link');
 
-    for (const link in linksNavbar) {
+    linksNavbar.forEach((link) => {
         const atributoHref = link.getAttribute('href').toLowerCase();
 
         if (enderecoAtual === atributoHref || enderecoAtual.startsWith(atributoHref + "/")) {
             link.classList.remove('nav-link', 'text-primary');
-            link.classList.add('btn', 'btn-primary', 'rounded-4');
+            link.classList.add('btn', 'btn-primary', 'rounded-3');
         } else {
-            link.classList.remove('btn', 'btn-primary', 'rounded-4');
+            link.classList.remove('btn', 'btn-primary', 'rounded-3');
             link.classList.add('nav-link', 'text-primary');
         }
-    }
+    });
 });
