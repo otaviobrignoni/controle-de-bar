686efde43cd35e7a48953ac5e248953b7874cb68
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-06-11 13:29:58 -0300

    Implementado cadastro, edição e exclusão de Produtos

--- ControleDeBar.WebApp/Controllers/ProdutoController.cs
+++ ControleDeBar.WebApp/Controllers/ProdutoController.cs
@@ -1,6 +1,7 @@
 ﻿using ControleDeBar.Dominio.ModuloProduto;
 using ControleDeBar.Infraestrura.Arquivos.Compartilhado;
 using ControleDeBar.Infraestrutura.Arquivos.ModuloProduto;
+using ControleDeBar.WebApp.Extensions;
 using ControleDeBar.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +19,7 @@ public class ProdutoController : Controller
         repositorioProduto = new RepositorioProdutoEmArquivo(contextoDados);
     }
 
+    [HttpGet]
     public IActionResult Index()
     {
         var registros = repositorioProduto.SelecionarRegistros();
@@ -25,5 +27,95 @@ public class ProdutoController : Controller
         var visualizarVM = new VisualizarProdutosViewModel(registros);
 
         return View(visualizarVM);
+    }
+
+    [HttpGet("cadastrar")]
+    public IActionResult Cadastrar()
+    {
+        var cadastrarVM = new CadastrarProdutoViewModel();
+
+        return View(cadastrarVM);
+    }
+
+    [HttpPost("cadastrar")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Cadastrar(CadastrarProdutoViewModel cadastrarVM)
+    {
+        var registros = repositorioProduto.SelecionarRegistros();
+
+        foreach (var item in registros)
+        {
+            if (item.Nome.Equals(cadastrarVM.Nome))
+            {
+                ModelState.AddModelError("CadastroUnico", "Já existe um produto registrado com este nome.");
+                break;
+            }
+        }
+
+        if (!ModelState.IsValid)
+            return View(cadastrarVM);
+
+        var entidade = cadastrarVM.ParaEntidade();
+
+        repositorioProduto.CadastrarRegistro(entidade);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet("editar/{id:guid}")]
+    public IActionResult Editar(Guid id)
+    {
+        var registroSelecionado = repositorioProduto.SelecionarRegistroPorId(id);
+
+        var editarVM = new EditarProdutoViewModel(
+            id,
+            registroSelecionado.Nome,
+            registroSelecionado.Valor
+        );
+
+        return View(editarVM);
+    }
+
+    [HttpPost("editar/{id:guid}")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Editar(Guid id, EditarProdutoViewModel editarVM)
+    {
+        var registros = repositorioProduto.SelecionarRegistros();
+
+        foreach (var item in registros)
+        {
+            if (!item.Id.Equals(id) && item.Nome.Equals(editarVM.Nome))
+            {
+                ModelState.AddModelError("CadastroUnico", "Já existe um produto registrado com este nome.");
+                break;
+            }
+        }
+
+        if (!ModelState.IsValid)
+            return View(editarVM);
+
+        var entidadeEditada = editarVM.ParaEntidade();
+
+        repositorioProduto.EditarRegistro(id, entidadeEditada);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet("excluir/{id:guid}")]
+    public IActionResult Excluir(Guid id)
+    {
+        var registroSelecionado = repositorioProduto.SelecionarRegistroPorId(id);
+
+        var excluirVM = new ExcluirProdutoViewModel(registroSelecionado.Id, registroSelecionado.Nome);
+
+        return View(excluirVM);
+    }
+
+    [HttpPost("excluir/{id:guid}")]
+    public IActionResult ExcluirConfirmado(Guid id)
+    {
+        repositorioProduto.ExcluirRegistro(id);
+
+        return RedirectToAction(nameof(Index));
     }
 }
